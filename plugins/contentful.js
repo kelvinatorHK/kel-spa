e46864bd075e519a883ba7e46318b872d4716a7b
Kelvin Lau <klau@nuskin.com>
2019-08-16 14:36:51 -0600

    added more stuff

--- plugins/contentful.js
+++ plugins/contentful.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const contentful = require('contentful')
-
+console.log("In contentfu.js");
+console.log("SPACE:", process.env.CTF_SPACE_ID);
 module.exports = contentful.createClient({
     space: process.env.CTF_SPACE_ID,
     accessToken: process.env.CTF_CD_ACCESS_TOKEN
