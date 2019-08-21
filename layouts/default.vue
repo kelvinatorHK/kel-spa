3818b6745b8861da6a7ad5e0aae4dfe4677d6307
Kelvin Lau <klau@nuskin.com>
2019-08-21 11:52:43 -0600

    Making it looks like Nuskin page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,60 +1,28 @@
 <template>
   <div>
-    <h1 class="site-title">
-        <nuxt-link :to="{ path: '/' }" class="site-title__link">
-            Eric branch
-        </nuxt-link>
-    </h1>
+    <ns-header></ns-header>
     <nuxt />
+    <ns-footer></ns-footer>
   </div>
 </template>
 
 <style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
+    html, body {
+        overscroll-behavior: none;
+        /* TODO: why is this not working in common-transition-styles? */
+        margin: 0;
+    }
+    .container{
+        margin: 40px auto 0;
+    }
+    @media (min-width: 1024px) {
+        .container{
+            width: 994px;
+        }
+    }
+    @media (min-width: 1440px) {
+        .container{
+            width: 1410px;
+        }
+    }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
     <h2>Latest posts</h2>
+    <br>
     <ul>
         <li v-if="posts" v-for="(post, index) in posts" :key="index">
             <nuxt-link :to="post.fields.slug">{{post.fields.description}}</nuxt-link>
@@ -23,35 +24,3 @@ export default {
 }
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
