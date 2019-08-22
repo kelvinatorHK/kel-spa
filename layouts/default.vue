ea9a7512f45b0ba4a18c120287993782da09fb21
Kelvin Lau <klau@nuskin.com>
2019-08-22 11:04:33 -0600

    Anthony test

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <div>
     <h1 class="site-title">
         <nuxt-link :to="{ path: '/' }" class="site-title__link">
-            Eric Demo #2
+            Anthony Demo
         </nuxt-link>
     </h1>
     <nuxt />
