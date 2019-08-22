84ab58ae8372b57fe1f0ab32b718d298ae824375
Kelvin Lau <klau@nuskin.com>
2019-08-22 16:02:30 -0600

    testing

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <div>
     <h1 class="site-title">
         <nuxt-link :to="{ path: '/' }" class="site-title__link">
-            Eric Demo #2
+            Eric Demo #3
         </nuxt-link>
     </h1>
     <nuxt />
