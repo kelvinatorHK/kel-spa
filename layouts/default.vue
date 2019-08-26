5f88807016f20eb8f04f7b480be8f396f77d2aa5
Kelvin Lau <klau@nuskin.com>
2019-08-26 15:04:05 -0600

    changed to use AEM

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <div>
     <h1 class="site-title">
         <nuxt-link :to="{ path: '/' }" class="site-title__link">
-            Eric Demo #3
+            AEM demo
         </nuxt-link>
     </h1>
     <nuxt />
